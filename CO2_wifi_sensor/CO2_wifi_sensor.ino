67fa944b2afcf1aa8ff7cdf5f822e6ae55403699
iberotecno <info@iberotecno.com>
2020-08-31 16:54:06 +0200

    Initial commit
    Updated software

---
+++ CO2_wifi_sensor/CO2_wifi_sensor.ino
@@ -0,0 +1,255 @@
+#include <WiFi.h>
+#include "SPIFFS.h"
+#include <ESPAsyncWebServer.h>       //https://github.com/alanswx/ESPAsyncWiFiManager
+#include <ESPAsyncWiFiManager.h>     //https://github.com/alanswx/ESPAsyncWiFiManager
+#include <ArduinoJson.h>             //https://github.com/bblanchon/ArduinoJson
+#include <SimpleTimer.h>             //https://github.com/jfturcot/SimpleTimer
+#include <FastLED.h>                 //https://github.com/FastLED/FastLED
+#include "ThingSpeak.h"              //https://github.com/mathworks/thingspeak-arduino/
+#include <FS.h>                      //library to access the filesystem
+
+#define LED_PIN     14
+#define NUM_LEDS    1
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+#define UPDATES_PER_SECOND 100
+float t = 0;
+CRGB leds[NUM_LEDS];
+
+//define custom fields
+char limite_malo[100];
+char limite_bueno[100];
+char brillo_led[100];
+char api_key[100];
+char numero_canal[100];
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+AsyncWebServer server(80);
+DNSServer dns;
+SimpleTimer timer;
+
+//Paleta de color
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+
+
+//datos thingspeak
+unsigned long myChannelNumber = 1122174; //update
+const char * myWriteAPIKey = "27F2DUNCV3LVYAY9"; //update
+
+void setup() {
+
+  Serial.begin(115200);
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+
+  currentPalette = RainbowColors_p;
+  currentBlending = LINEARBLEND;
+
+  //FS
+  Serial.println("\n Starting");
+  //
+  //***clean FS, for testing
+  //SPIFFS.format();
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+          strcpy(limite_malo, json["limite_malo"]);
+          strcpy(limite_bueno, json["limite_bueno"]);
+          strcpy(brillo_led, json["brillo_led"]);
+          strcpy(api_key, json["api_key"]);
+          strcpy(numero_canal, json["numero_canal"]);
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+
+  //end read
+  AsyncWiFiManagerParameter custom_limite_bueno("limite_bueno", "Valor aire bueno", limite_bueno, 100);
+  AsyncWiFiManagerParameter custom_limite_malo("limite_malo", "Valor aire malo", limite_malo, 100);
+  AsyncWiFiManagerParameter custom_brillo_led("brillo_led", "Brillo del LED", brillo_led, 100);
+  AsyncWiFiManagerParameter custom_api_key("api_key", "API Key", api_key, 100);
+  AsyncWiFiManagerParameter custom_numero_canal("numero_canal", "Channel ID", numero_canal, 100);
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  AsyncWiFiManager wifiManager(&server, &dns);
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  wifiManager.addParameter(&custom_limite_bueno);
+  wifiManager.addParameter(&custom_limite_malo);
+  wifiManager.addParameter(&custom_brillo_led);
+  wifiManager.addParameter(&custom_api_key);
+  wifiManager.addParameter(&custom_numero_canal);
+
+  //scheduled reset
+  int horas = 1; // hours for scheduled reset
+  timer.setInterval(horas * 3600000, reinicio);
+
+  //WiFiManager
+  AsyncWiFiManagerParameter custom_text("<p>Seleccione la red WiFi para conectarse.</p>");
+  wifiManager.addParameter(&custom_text);
+  //reset saved settings
+  //  wifiManager.resetSettings();
+  //  WiFi.disconnect(true);   // still not erasing the ssid/pw. Will happily reconnect on next start
+  //  WiFi.begin("0","0");       // adding this effectively seems to erase the previous stored SSID/PW
+  //  ESP.restart();
+  //  delay(1000);
+  //set custom ip for portal
+  //wifiManager.setAPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+  //fetches ssid and pass from eeprom and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  wifiManager.autoConnect("Iberotecno AP");
+  //or use this for auto generated name ESP + ChipID
+  //wifiManager.autoConnect();
+  //if you get here you have connected to the WiFi
+  Serial.println("connected...yeey :)");
+
+
+  //read updated parameters
+  strcpy(limite_malo, custom_limite_malo.getValue());
+  strcpy(limite_bueno, custom_limite_bueno.getValue());
+  strcpy(brillo_led, custom_brillo_led.getValue());
+  strcpy(api_key, custom_api_key.getValue());
+  strcpy(numero_canal, custom_numero_canal.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["limite_malo"] = limite_malo;
+    json["limite_bueno"] = limite_bueno;
+    json["brillo_led"] = brillo_led;
+    json["api_key"] = api_key;
+    json["numero_canal"] = numero_canal;
+
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+  Serial.println("local ip");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  WiFiClient client;
+  //WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client); // Initialize ThingSpeak
+
+  timer.run();
+  int brillo_led_convertido = atoi(brillo_led);
+  int limite_malo_convertido = atoi(limite_malo);
+  int limite_bueno_convertido = atoi(limite_bueno);
+
+  unsigned long numero_canal_convertido = strtoul(numero_canal, NULL, 10);
+
+  FastLED.setBrightness(brillo_led_convertido);
+
+  int t = analogRead(A0);  // Read sensor value and stores in a variable t
+  Serial.print("Airquality = ");
+  Serial.println(t);
+
+  if (t <= limite_bueno_convertido)
+  {
+    FastLED.setBrightness(brillo_led_convertido);
+    fill_solid(leds, NUM_LEDS, CRGB::Blue);
+    FastLED.show();
+    Serial.println("Aire puro");
+  }
+  else if ( t >= limite_bueno_convertido && t <= limite_malo_convertido )
+  {
+    FastLED.setBrightness(brillo_led_convertido);
+    fill_solid(leds, NUM_LEDS, CRGB::Yellow);
+    FastLED.show();
+    Serial.println("Aire cargandose");
+  }
+  else if (t >= limite_malo_convertido )
+  {
+    FastLED.setBrightness(brillo_led_convertido);
+    fill_solid(leds, NUM_LEDS, CRGB::Red);
+    FastLED.show();
+    Serial.println("Aire viciado");
+  }
+
+  ThingSpeak.setField(1, t); //co2, ppm
+
+  //debug
+  //Serial.print("Numero canal char: ");
+  //Serial.println(numero_canal);
+  //Serial.print("Numero canal convertido: ");
+  //Serial.println(numero_canal_convertido);
+  //Serial.print("API Key: ");
+  //Serial.println(api_key);
+  //Serial.print("Limites bueno y malo : ");
+  //Serial.println(limite_bueno);
+  //Serial.println(limite_bueno_convertido);
+
+
+  // write to the ThingSpeak channel
+  //int x = ThingSpeak.writeFields(numero_canal_convertido, api_key);
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+
+  delay(15000); // Wait 20 seconds to update the channel again
+
+  if (x == 200) {
+    Serial.println("Channel update successful.");
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
+    delay(500);
+  }
+  else {
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+
+  }
+
+
+}
+
+// restart function definition
+void reinicio() {
+  ESP.restart();
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+IoT Water Sensor
+=======
+
+Telegram water sensor for ESP32.
+
+This example sends a telegram message from te ESP32 board to a cell phone with Telegram App installed when a water leakage is detected by the sensor. Youn only need to copy the lib content to your arduino libraryes folder and run the program that you find at src folder.
+
+You will need to install Arduino IDE: https://iberotecno.com/blog-supratecno/47-tutoriales-paso-a-paso/107-como-instalar-y-configurar-el-ide-de-arduino-tutorial
+
+And the requiered dependencies for ESP32 boards: https://iberotecno.com/blog-supratecno/47-tutoriales-paso-a-paso/108-como-instalar-esp32-en-el-ide-de-arduino-tutorial
+
+Thanks and enjoy!!
